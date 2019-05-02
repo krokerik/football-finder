970097fb20dbc5f7e3465d6ec3de90f7a894b4f2
Erik Andersson <erik_andersson007@hotmail.com>
2019-05-02 16:29:40 +0200

    print information about games and venues

--- script.js
+++ script.js
@@ -25,7 +25,13 @@ var districts = [{name: "Blekinge Fotbollsförbund",  id: 2},
                  {name: "Östergötlands FF", id: 24},
                  {name: "FIFA", id: 27}];
 
+var venues = [];
+
+var leagues = [];
+
 var games = [];
+
+var promises = [];
 
 function setup() {
 	var select = document.getElementById("districtPicker");
@@ -59,28 +65,26 @@ function getGames() {
 		return gameTable.children;
 	}).then(function(rows) {
 		games.length = 0; // empty information array
+		venues.length = 0;
 		var league = "";
+		var leagueId = -1;
 		for(var i=0; i<rows.length; i++) {
 			var element = rows[i];
 			if(element.children.length == 1) {
 				league = element.children[0].children[0].firstChild.nodeValue;
-			} else if(element.children.length == 4) {
+				leagueID = element.children[0].children[0].firstChild.nodeValue;
+			} else if(element.children.length == 4 && element.children[0].firstChild.hasChildNodes()) {
 				var time = element.children[0].firstChild.firstChild.nodeValue;
 				var home = element.children[1].firstChild.firstChild.nodeValue.split(" - ")[0];
 				var away = element.children[1].firstChild.firstChild.nodeValue.split(" - ")[1];
 				var venue = element.children[3].firstChild.firstChild.nodeValue;
-				var id = element.children[3].firstChild.href.split("&faid=")[1];
-				var game = {
-				            	home: home,
-				            	away: away,
-				            	venue: {name: venue, id: id},
-				            	time: time,
-				            	league: league
-				};
-				games.push(game);
+				var VenueId = element.children[3].firstChild.href.split("&faid=")[1];
+
+				addGame(time,home,away,{name: venue, id: VenueId, games: []},league);
 			}
 		}
 		loaded();
+		renderList();
 	}).catch(function(e) {
 		loaded();
 		console.error("Error:",e);
@@ -97,27 +101,93 @@ function loaded() {
 	settingsButton.disabled = false;
 }
 
-function getVenuePositions() {
-	var checked = [];
+function addGame(time,home,away,venue,league) {
+	var exists = venues.findIndex(x => x.id === venue.id);
+	if(exists!=-1) {
+		venue = venues[exists];
+	} else {
+		promises.push(getPosition(venue));
+		Promise.all(promises).then(function() {
+			renderVenueList();
+		});
+		venues.push(venue);
+	}
+	var game = {
+		home: home,
+		away: away,
+		venue: venue,
+		time: time,
+		league: league
+	};
+	venue.games.push(game);
+	games.push(game);
+}
+
+function getPosition(venue) {
+	loading();
+	var url = "https://cors-anywhere.herokuapp.com/http://fogismallar2.svenskfotboll.se/ft.aspx?scr=venue&faid="+venue.id;
+	return fetch(url).then(function(response) {
+		return response.text();
+	}).then(function(plain) {
+		var parser = new DOMParser();
+		var remoteDocument = parser.parseFromString(plain,"text/html");
+		var mapLink = remoteDocument.documentElement.querySelector("a[href^=\"http://maps.google.com\"]");
+		if(mapLink) {
+			venue.lat = mapLink.href.split("http://maps.google.com/maps?q=")[1].split(",")[0];
+			venue.lon = mapLink.href.split("http://maps.google.com/maps?q=")[1].split(",")[1];
+		}
+	});
+}
+
+function renderList() {
+	var list = document.getElementById("gameList");
+	var child = list.firstElementChild;
+	while(child) {
+		list.removeChild(child);
+		child = list.firstElementChild;
+	}
 	for(var i=0; i<games.length; i++) {
 		var game = games[i];
-		if(!checked.includes(game.venue.id)) {
-			checked.push[game.venue.id];
-			var url = "https://cors-anywhere.herokuapp.com/http://fogismallar2.svenskfotboll.se/ft.aspx?scr=venue&faid="+game.venue.id;
-			fetch(url).then(function(response) {
-				return response.text();
-			}).then(function(plain) {
-				var parser = new DOMParser();
-				var remoteDocument = parser.parseFromString(plain, "text/html");
-				var mapLink = remoteDocument.documentElement.querySelector("a[href^=\"http://maps.google.com\"]");
-				if(mapLink) {
-					var lat = mapLink.href.split("http://maps.google.com/maps?q=")[1].split(",")[0];
-					var lon = mapLink.href.split("http://maps.google.com/maps?q=")[1].split(",")[1];
-					game.venue.lat = lat;
-					game.venue.lon = lon;
-				}
-			});
-		}
+		var element = document.createElement("li");
+		var text = document.createTextNode("Game: "+game.home+" - "+game.away+" @ "+game.venue.name);
+		element.appendChild(text);
+		list.appendChild(element);
 	}
 }
+
+function renderVenueList() {
+	var list = document.getElementById("venueList");
+	var child = list.firstElementChild;
+	while(child) {
+		list.removeChild(child);
+		child = list.firstElementChild;
+	}
+	venues.sort(function(a,b) {
+		if(a.games.length > b.games.length) {
+			return -1;
+		}
+		if(a.games.length < b.games.length) {
+			return 1;
+		}
+		return 0;
+	});
+	for(var i=0; i<venues.length; i++) {
+		var venue = venues[i];
+		var element = document.createElement("li");
+		var text = document.createTextNode(venue.name + " - "+venue.games.length+" games");
+		console.log(typeof(venue.lat) !== "undefined");
+		console.log(typeof(venue.lat) +" !== "+ "undefined");
+		console.log(venue.lat);
+		if(typeof(venue.lat) !== "undefined" && typeof(venue.lon) !== "undefined") {
+			var link = document.createElement("a");
+			link.href = "http://maps.google.com/maps?q="+venue.lat+","+venue.lon;
+			link.appendChild(text);
+			element.appendChild(link);
+		} else {
+			element.appendChild(text);
+		}
+		list.appendChild(element);
+	}
+}
+
 window.onload = setup;
